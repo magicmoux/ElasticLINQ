9772960c1aa9033cdccfb1de1a10ae34987cd563
Max <max@micrologiciel.fr>
2018-02-26 09:43:28 +0100

    Testing prerelase into specific repo

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
 [assembly: AssemblyCopyright("Copyright (C) 2013-2017 CenturyLink, Inc.")]
-
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.4.3.0")]
 [assembly: AssemblyFileVersion("1.4.3.0")]
+[assembly: AssemblyInformationalVersion("1.4.3.0-fork")]
