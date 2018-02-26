83e9db4fdd0c63a93cebf6b34ba4b65bbd69e5d4
magicmoux <magicmoux@gmail.com>
2018-02-26 14:30:58 +0100

    Replaced by GitVersion

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ElasticLINQ")]
 [assembly: AssemblyCopyright("Copyright (C) 2013-2017 CenturyLink, Inc.")]
-
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.3.0")]
-[assembly: AssemblyFileVersion("1.4.3.0")]
+//[assembly: AssemblyVersion("1.4.3.0")]
+//[assembly: AssemblyFileVersion("1.4.3.0")]
