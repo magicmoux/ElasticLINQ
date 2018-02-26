464e0fc7bf690eb728606b64ae264744c840b0d7
magicmoux <magicmoux@gmail.com>
2018-02-26 10:14:23 +0100

    Renaming product

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("ElasticLINQ")]
+[assembly: AssemblyProduct("ML.ElasticLINQ")]
 [assembly: AssemblyCopyright("Copyright (C) 2013-2017 CenturyLink, Inc.")]
 [assembly: ComVisible(false)]
 //[assembly: AssemblyVersion("1.4.3.0")]
